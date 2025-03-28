7b64e874c03ce44ec421125c3f46674fbc1440f4
ivkumar <40355285+ivkumar@users.noreply.github.com>
2025-03-28 21:07:27 +0530

    testing commit
    testing commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2313025758"
+    serviceWorkerVersion: "784549814"
   }
 });
